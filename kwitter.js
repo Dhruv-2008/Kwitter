efc9b15e3c724c6283040bd8770485c333d9c163
Dhruv-2008 <79134023+Dhruv-2008@users.noreply.github.com>
2021-06-21 10:38:45 +0530

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -3,3 +3,12 @@ function addUser(e){
     var user_name=document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
 }
+
+firebase.initializeApp({
+  // Your firebase configuration object
+});
+
+const appCheck = firebase.appCheck();
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+appCheck.activate('abcdefghijklmnopqrstuvwxy-1234567890abcd');
